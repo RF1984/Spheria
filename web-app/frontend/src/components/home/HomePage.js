8f2220eb07508476eb00b68f6fce0668a33a3b8b
Dejan Jakovchevski <dexapage@gmail.com>
2019-12-02 12:09:49 +0100

    restuctureFolders

--- web-app/frontend/src/components/home/HomePage.js
+++ web-app/frontend/src/components/home/HomePage.js
@@ -1,13 +1,33 @@
+
 import React, { Component } from "react";
-import '../components.css';
 
 class HomePage extends Component {
     render() {
         return (
-            <div className="card-hp">
-                <div className="card-bodyhp">
-                <img src ={"/images/logo_transparent.png"} width="500" height="500"/>
+            <div>
+                <br/>
+                <img src="spinning-top.svg" class="rounded float-left" width="90" height="90"alt="logo"/>
+                <img src="parenthood.svg" class="rounded float-right" width="90" height="90"alt="logo"/>
+                <p></p>
+                <div>
+                <center>       
+                    <p className="big-p">
+                        Here you can find your new toy!
+                        <br />
+                        Try serch!
+                    </p>
+                    <div class="inner-form">
+                    <div class="input-field first-wrap">
+                        <input id="search" type="text" placeholder="Find your toy" />
+                    </div>
+                     
+                    <div class="input-field third-wrap">
+                        <button class="btn-search" type="button">Search</button>
+                    </div>
+                </div>               
+                </center>
                 </div>
+                
             </div>
         );
     }
