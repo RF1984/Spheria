680c63c1e4d242e3edb897586a16e96cf7e37a96
RF1984 <rafaeldcfernandes@gmail.com>
2019-12-04 18:11:05 +0100

    createt random currency conversion

--- web-app/frontend/src/components/home/Curency.js
+++ web-app/frontend/src/components/home/Curency.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 class Convert extends React.Component {
     state = {
         rates: null,
-        randomCurrency: null,
+        randomCurrency: {},
     };
 async componentDidMount(){
 const url = "https://api.exchangeratesapi.io/latest?base=SEK";
@@ -11,17 +11,28 @@ const response = await Axios.get(url);
 this.setState({ rates: response.data.rates,loading: false });
 console.log(response)
 
+const EUR = {country: 'France', code: 'EUR', text: 'euros'};
+const INR = {country: 'India', code: 'INR', text: 'rupees'};
+const ILS = {country: 'Palestina', code: 'ILS', text: 'israel Shekels'}
+const PLN = {country: 'Poland', code: 'PLN', text: 'poland zloty'}
+const PKR = {country: 'Pakistan',code: 'PKR', text: 'pakistan rupees'}
+const RON = {country: 'Romania', code: 'RON', text: 'romanian leu'}
+const RUB = {country: 'Russia', code: 'RUB', text: 'russian rubles'}
 
-let currencies = ['EUR','INR']
+
+let currencies = [EUR,INR,ILS,PLN,RON,RUB]
+console.log(currencies[Math.floor(Math.random() * currencies.length)]);
+
 this.setState({randomCurrency: currencies[Math.floor(Math.random() * currencies.length)]});
 
 
 }
 render() {
     const {randomCurrency} = this.state
-    const convertedPrice= this.state.rates ? this.props.price * this.state.rates[randomCurrency] : "";
+    console.log(this.state)
+    const convertedPrice= this.state.rates ? this.props.price * this.state.rates[randomCurrency.code] : "";
     return <div>
-        {this.state.rates ? <div>the currency in robel: {convertedPrice}</div> : <div>working..</div>}
+        {this.state.rates ? <div>in {randomCurrency.country} this is worth: {convertedPrice.toFixed(2)} {randomCurrency.text} </div> : <div>working..</div>}
     </div>
 }
 }
